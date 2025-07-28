f5e422dc6b22ff413f59898de8d83f4494472a13
Lcrescent3es <crescent3es@gmail.com>
2025-07-29 03:07:53 +0900

    chore: warnings resolved

--- src/main/java/com/nocktice/nocktice/alarm/controller/AlarmController.java
+++ src/main/java/com/nocktice/nocktice/alarm/controller/AlarmController.java
@@ -4,7 +4,6 @@ import com.nocktice.nocktice.alarm.dto.AlarmRequestDto;
 import com.nocktice.nocktice.alarm.service.AlarmService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
